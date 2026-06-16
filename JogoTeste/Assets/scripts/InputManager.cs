2b7442e1fd755d5c0ab83610e5797063a19d8251
matheussaur-v1 <20241ctb0180016@estudantes.ifpr.edu.br>
2026-06-16 17:08:20 -0300

    mexidao1.5

---
+++ JogoTeste/Assets/scripts/InputManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerControls playerControls;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    private void Onable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public Vector2 GetPlayerMovement()
+    {
+        return playerControls.Player.Move.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetMouseDelta()
+    {
+        return playerControls.Player.Look.ReadValue<Vector2>();
+    }
+
+    public bool PlayerJumpedThsFrame()
+    {
+        return playerControls.Player.Jump.triggered;
+    }
+}
